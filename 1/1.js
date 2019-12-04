5e71462daea68f24c6caa9f00444121851920958
turana1998 <turana221298@gmail.com>
2019-12-04 21:14:29 +0400

    Calculator

---
+++ 1/1.js
@@ -0,0 +1,5 @@
+document.write("salam");
+console.log("salam");
+alert("salam");
+document.getElementById("hmm").innerHTML="<b>salam</b>"
+document.getElementById("hmm").innerText="<b>salam</b>";
---
+++ 1/2.js
@@ -0,0 +1,8 @@
+//let name="Mahir";
+//alert(`salam ${name}`);
+function salamla()
+{
+    let name=document.getElementById("usr").value;
+    alert(`salam ${name}`);
+
+}
